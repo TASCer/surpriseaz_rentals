dc0596ae535756bddb8d73b42355ccd539b1eca5
TASCer <me@example.com>
2024-02-27 09:05:08 -0700

    re-create rentals tbl

--- hoa_insights/src/update_parcel_data.py
+++ hoa_insights/src/update_parcel_data.py
@@ -73,86 +73,89 @@ def update(latest_data):
 			f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB_HOSTNAME}/{DB_NAME}')
 	except (exc.OperationalError, AttributeError) as e:
 		logger.error(str(e))
+	
+	with engine.connect() as conn, conn.begin():
+		delete_rentals = f"DELETE FROM {DB_NAME}.{PARCEL_RENTALS};"
+		conn.execute(text(delete_rentals))
+	
+		for parcel_details in latest_data:
+			if not parcel_details['Owner']:
+				apn: str = parse_apn(parcel_details["TreasurersTransitionUrl"].split('='))
+				owner, mail_to, deed_type = 3 * ('',)
+				is_rental: bool = parcel_details['IsRental']
+				last_legal_class: str = parcel_details['Valuations'][0]['LegalClassificationCode']
+				deed_date = parse('1901-01-01')
+				sales_price = 0
+				logger.warning(f"No Owner Identified!! {apn}")
 
-	for parcel_details in latest_data:
-		if not parcel_details['Owner']:
-			apn: str = parse_apn(parcel_details["TreasurersTransitionUrl"].split('='))
-			owner, mail_to, deed_type = 3 * ('',)
-			is_rental: bool = parcel_details['IsRental']
-			last_legal_class: str = parcel_details['Valuations'][0]['LegalClassificationCode']
-			deed_date = parse('1901-01-01')
-			sales_price = 0
-			logger.warning(f"No Owner Identified!! {apn}")
+			elif parcel_details['Owner']:
+				apn: str = parse_apn(parcel_details["TreasurersTransitionUrl"].split('=')[1])
+				deed_date: str = parse_date(parcel_details['Owner']['DeedDate'])
+				deed_type: str = parcel_details['Owner']['DeedType']
 
-		elif parcel_details['Owner']:
-			apn: str = parse_apn(parcel_details["TreasurersTransitionUrl"].split('=')[1])
-			deed_date: str = parse_date(parcel_details['Owner']['DeedDate'])
-			deed_type: str = parcel_details['Owner']['DeedType']
+				if not deed_type:
+						deed_type = ''
 
-			if not deed_type:
-					deed_type = ''
+				mail_to: str = parcel_details['Owner']['FullMailingAddress']
+				mail_to: str = mail_to.replace(',', '')
+				owner: str = parcel_details['Owner']['Ownership']
 
-			mail_to: str = parcel_details['Owner']['FullMailingAddress']
-			mail_to: str = mail_to.replace(',', '')
-			owner: str = parcel_details['Owner']['Ownership']
+				if "'" in owner:
+					owner = owner.replace("'", "''")
 
-			if "'" in owner:
-				owner = owner.replace("'", "''")
+				is_rental: int = int(parcel_details['IsRental'])
+				last_legal_class: str = parcel_details['Valuations'][0]['LegalClassificationCode']
+				sale_date: str = parse_date(parcel_details['Owner']['SaleDate'])
+				sale_price: str = parcel_details['Owner']['SalePrice']
 
-			is_rental: int = int(parcel_details['IsRental'])
-			last_legal_class: str = parcel_details['Valuations'][0]['LegalClassificationCode']
-			sale_date: str = parse_date(parcel_details['Owner']['SaleDate'])
-			sale_price: str = parcel_details['Owner']['SalePrice']
-
-			if sale_price is None:
-				sale_price = 0
-
-		try:
-			with engine.connect() as conn, conn.begin():
-				insert_qry = (f"INSERT INTO hoa_insights.{PARCEL_OWNERS} (APN, OWNER, MAIL_ADX, RENTAL, SALE_DATE, SALE_PRICE, DEED_DATE, DEED_TYPE, LEGAL_CODE) "
-							  f"VALUES('{apn}', '{owner}', '{mail_to}', '{is_rental}', '{sale_date}', '{sale_price}','{deed_date}', '{deed_type}', '{last_legal_class}') "
-							  f"ON DUPLICATE KEY UPDATE  OWNER='{owner}',MAIL_ADX='{mail_to}',RENTAL='{is_rental}', SALE_DATE='{sale_date}', SALE_PRICE='{sale_price}', DEED_DATE='{deed_date}',DEED_TYPE='{deed_type}', LEGAL_CODE='{last_legal_class}';")
-				conn.execute(text(insert_qry))
-
-		except (exc.OperationalError, exc.ProgrammingError, UnboundLocalError) as e:
-			logger.error(e)
-
-		if is_rental:
-			rental_owner_type: str = parcel_details['RentalInformation']['OwnershipType']
-			rental_owner_name: str = parcel_details['RentalInformation']['OwnerName']
-			rental_owner_address: str = parcel_details['RentalInformation']['OwnerAddress']
-			rental_owner_address: str = rental_owner_address.replace(',', ' ')
-			rental_owner_phone: str = parse_ph_nums(parcel_details['RentalInformation']['OwnerPhone'])
-
-			if isinstance(rental_owner_name, str):
-				rental_owner_name: str = rental_owner_name.replace(',', ' ')
-			else:
-				rental_owner_name: str = parcel_details['RentalInformation']['OwnerName']['Name']
-				rental_owner_name: str = rental_owner_name.replace(',', ' ')
-
-			if parcel_details['RentalInformation']['AgentName']:
-				rental_contact_name: str = parcel_details['RentalInformation']['AgentName']
-				rental_contact_name: str = rental_contact_name.replace(',', '')
-				rental_contact_address: str = parcel_details['RentalInformation']['AgentAddress']
-				rental_contact_address: str = rental_contact_address.replace(',', '')
-				rental_contact_phone: str = parse_ph_nums(parcel_details['RentalInformation']['AgentPhone'])
-
-			elif parcel_details['RentalInformation']['BusinessContactName']:
-				rental_contact_name: str = parcel_details['RentalInformation']['BusinessContactName']
-				rental_contact_name = rental_contact_name.replace(',', '')
-				rental_contact_address: str = parcel_details['RentalInformation']['BusinessContactAddress']
-				rental_contact_address: str = rental_contact_address.replace(',', '')
-				rental_contact_phone: str = parse_ph_nums(parcel_details['RentalInformation']['BusinessContactPhone'])
-			else:
-				rental_contact_name: str = rental_owner_name
-				rental_contact_address: str = rental_owner_address
-				rental_contact_phone: str = rental_owner_phone
+				if sale_price is None:
+					sale_price = 0
 
 			try:
-				with engine.connect() as conn, conn.begin():
-					insert_qry = (f"""INSERT INTO hoa_insights.{PARCEL_RENTALS} (APN, OWNER, OWNER_TYPE, CONTACT, CONTACT_ADX, CONTACT_PH)
-								  VALUES('{apn}', '{rental_owner_name}', '{rental_owner_type}', '{rental_contact_name}', '{rental_contact_address}', '{rental_contact_phone}')
-								  ON DUPLICATE KEY UPDATE OWNER='{rental_owner_name}', OWNER_TYPE='{rental_owner_type}', CONTACT='{rental_contact_name}', CONTACT_ADX='{rental_contact_address}', CONTACT_PH='{rental_contact_phone}';""")
-					conn.execute(text(insert_qry))
-			except exc.OperationalError as e:
+				insert_qry = (f"INSERT INTO hoa_insights.{PARCEL_OWNERS} (APN, OWNER, MAIL_ADX, RENTAL, SALE_DATE, SALE_PRICE, DEED_DATE, DEED_TYPE, LEGAL_CODE) "
+						f"VALUES('{apn}', '{owner}', '{mail_to}', '{is_rental}', '{sale_date}', '{sale_price}','{deed_date}', '{deed_type}', '{last_legal_class}') "
+						f"ON DUPLICATE KEY UPDATE  OWNER='{owner}',MAIL_ADX='{mail_to}',RENTAL='{is_rental}', SALE_DATE='{sale_date}', SALE_PRICE='{sale_price}', DEED_DATE='{deed_date}',DEED_TYPE='{deed_type}', LEGAL_CODE='{last_legal_class}';")
+				conn.execute(text(insert_qry))
+
+			except (exc.OperationalError, exc.ProgrammingError, UnboundLocalError) as e:
 				logger.error(e)
+
+			if is_rental:
+				rental_owner_type: str = parcel_details['RentalInformation']['OwnershipType']
+				rental_owner_name: str = parcel_details['RentalInformation']['OwnerName']
+				rental_owner_address: str = parcel_details['RentalInformation']['OwnerAddress']
+				rental_owner_address: str = rental_owner_address.replace(',', ' ')
+				rental_owner_phone: str = parse_ph_nums(parcel_details['RentalInformation']['OwnerPhone'])
+
+				if isinstance(rental_owner_name, str):
+					rental_owner_name: str = rental_owner_name.replace(',', ' ')
+				else:
+					rental_owner_name: str = parcel_details['RentalInformation']['OwnerName']['Name']
+					rental_owner_name: str = rental_owner_name.replace(',', ' ')
+
+				if parcel_details['RentalInformation']['AgentName']:
+					rental_contact_name: str = parcel_details['RentalInformation']['AgentName']
+					rental_contact_name: str = rental_contact_name.replace(',', '')
+					rental_contact_address: str = parcel_details['RentalInformation']['AgentAddress']
+					rental_contact_address: str = rental_contact_address.replace(',', '')
+					rental_contact_phone: str = parse_ph_nums(parcel_details['RentalInformation']['AgentPhone'])
+
+				elif parcel_details['RentalInformation']['BusinessContactName']:
+					rental_contact_name: str = parcel_details['RentalInformation']['BusinessContactName']
+					rental_contact_name = rental_contact_name.replace(',', '')
+					rental_contact_address: str = parcel_details['RentalInformation']['BusinessContactAddress']
+					rental_contact_address: str = rental_contact_address.replace(',', '')
+					rental_contact_phone: str = parse_ph_nums(parcel_details['RentalInformation']['BusinessContactPhone'])
+				else:
+					rental_contact_name: str = rental_owner_name
+					rental_contact_address: str = rental_owner_address
+					rental_contact_phone: str = rental_owner_phone
+
+				try:
+					insert_qry = (f"""INSERT INTO hoa_insights.{PARCEL_RENTALS} (APN, OWNER, OWNER_TYPE, CONTACT, CONTACT_ADX, CONTACT_PH)
+									VALUES('{apn}', '{rental_owner_name}', '{rental_owner_type}', '{rental_contact_name}', '{rental_contact_address}', '{rental_contact_phone}')
+									ON DUPLICATE KEY UPDATE OWNER='{rental_owner_name}', OWNER_TYPE='{rental_owner_type}', CONTACT='{rental_contact_name}', CONTACT_ADX='{rental_contact_address}', CONTACT_PH='{rental_contact_phone}';""")
+					conn.execute(text(insert_qry))
+					
+				except exc.OperationalError as e:
+					logger.error(e)
